83f9833ff0278091f4dad545fe84548fd6838d82
Stephen Newkirk <newkirk771@gmail.com>
2016-09-09 08:54:03 -0700

    Add contact class to contact.php and create all properties and functions of class.

---
+++ src/contact.php
@@ -0,0 +1,60 @@
+<?php
+    class Contact
+    {
+        private $name;
+        private $phone;
+        private $address;
+
+        function __construct($new_name, $new_phone, $new_address)
+        {
+            $this->name = $new_name;
+            $this->phone = $new_phone;
+            $this->address = $new_address;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getPhone()
+        {
+            return $this->phone;
+        }
+
+        function setPhone($new_phone)
+        {
+            $this->phone = (string) $new_phone;
+        }
+
+        function getAddress()
+        {
+            return $this->address;
+        }
+
+        function setAddress($new_address)
+        {
+            $this->address = (string) $new_address;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_contacts'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_albums'] = array();
+        }
+    }
+ ?>
