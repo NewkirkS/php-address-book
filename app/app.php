3bb731269143d0b1bccdfdbb7b25eb386fb7d87e
Stephen Newkirk <newkirk771@gmail.com>
2016-09-09 09:27:24 -0700

    Make route in app file to homepage.

--- app/app.php
+++ app/app.php
@@ -3,13 +3,18 @@
     require_once __DIR__."/../src/contact.php";
 
     session_start();
-    if(empty($_SESSION['list_of_contacts'])) {
-        $_SESSION['list_of_contacts'] = array();
+    if(empty($_SESSION["list_of_contacts"])) {
+        $_SESSION["list_of_contacts"] = array();
     }
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__.'/../views'
+        "twig.path" => __DIR__."/../views"
     ));
 
+    $app->get("/", function() use ($app) {
+        return $app["twig"]->render("address-home.html.twig", array("contacts" => Contact::getAll()));
+    });
+
+    
  ?>
